c867734e165396445b517cfb4cc0fcb794aca5ab
mihail.sitnic <mihail.sitnic@gmail.com>
2016-08-05 16:17:51 +0300

    refix card styles

--- source/components/app/app.js
+++ source/components/app/app.js
@@ -1,10 +1,11 @@
 import Topic from "../topic/topic.js";
 import Model from "../model/model.js";
+import Card from "../card/card.js";
 
 const topicOptions = {
 	el: document.querySelector(".card"),
 };
 
-const topic = new Topic(topicOptions);
+const card = new Card(topicOptions);
 const model = new Model();
 model.fetch();
