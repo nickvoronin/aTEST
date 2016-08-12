e0b1e95fa84952b4a9937ecbbb8f8766551e703c
palzuncoff <palzuncoff@yahoo.com>
2016-08-12 16:29:03 +0300

    chenge App.php setup.php

--- server/Classes/App.php
+++ server/Classes/App.php
@@ -39,8 +39,8 @@ class App {
             ); 
     /* fetch associative array */
     while ($row = $tables->fetch_assoc()) {
-      $card=$row['topic_id'];
-      $row['card']=  $this->view_cards($card);
+      $card=$row['topicId'];
+      $row['cards']=  $this->view_cards($card);
       $topics_array[]=$row;
     }
     /* free result set */
@@ -54,33 +54,48 @@ class App {
   public function view_cards($topic_id) {
     $tables=queryMysql(
             "SELECT * FROM cards "
-          . "WHERE parent_topic_id='$topic_id'"
-            ); 
+          . "WHERE parentTopicId='$topic_id'"
+            );
     /* fetch associative array */
     while ($row = $tables->fetch_assoc()) {
-      $alt=array(
-                'versions'=>explode('|', $row['versions']), 
-                'answer'=>explode('|', $row['answer'])
-                );
+      $parent_card_id=$row['cardId'];
+      $alt['responses']=$this->view_responses($parent_card_id);
       $rows=array_replace($row, $alt);
       $cards_array[]=$rows;
     }
-    
+
     /* free result set */
     $tables->free();
-    
+
     return $cards_array;
+  }
+  
+  
+  public function view_responses($card_id) {
+    $tables=queryMysql(
+            "SELECT * FROM responses "
+          . "WHERE parentCardId ='$card_id'"
+            );
+    /* fetch associative array */
+    while ($row = $tables->fetch_assoc()) {
+      $responses_array[]=$row;
+    }
+    /* free result set */
+    $tables->free();
+
+    return $responses_array;
+
   }
   
 
   public function add_topic($array) {
-    $query_topic_name=$array['topic_name'];
-    $query_topic_description=$array['topic_description'];
-    $query_topic_language=$array['topic_language'];
+    $query_topic_name=$array['topicName'];
+    $query_topic_description=$array['topicDescription'];
+    $query_topic_language=$array['topicLanguage'];
     $query_topic_user=$array['user'];
     $check_topic = queryMysql(
             "SELECT * FROM topics "
-          . "WHERE topic_name='$query_topic' "
+          . "WHERE topicName='$query_topic' "
           . "AND user='$query_topic_user'"
             );
     if (!$check_topic->num_rows) {
@@ -101,16 +116,16 @@ class App {
 
   
   public function edit_topic($array) {
-    $query_topic_name=$array['topic_name'];
-    $query_topic_description=$array['topic_description'];
-    $query_topic_language=$array['topic_language'];
-    $query_topic_id=$array['topic_id'];
+    $query_topic_name=$array['topicName'];
+    $query_topic_description=$array['topicDescription'];
+    $query_topic_language=$array['topicLanguage'];
+    $query_topic_id=$array['topicId'];
     queryMysql(
             "UPDATE topics "
-          . "SET topic_name='$query_topic_name',"
-          . "topic_description='$query_topic_description',"
-          . "topic_language='$query_topic_language'"
-          . "WHERE topic_id='$query_topic_id'"
+          . "SET topicName='$query_topic_name',"
+          . "topicDescription='$query_topic_description',"
+          . "topicLanguage='$query_topic_language'"
+          . "WHERE topicId='$query_topic_id'"
             );
     
     
@@ -121,7 +136,7 @@ class App {
   public function delete_topic($topic_id) {
     queryMysql(
             "DELETE FROM topics"
-          . "WHERE topic_id='$query_topic_id'"
+          . "WHERE topicId='$query_topic_id'"
             );
     
     return $this->viewTopics;
@@ -129,14 +144,13 @@ class App {
   
   
   public function add_card($array) {
-    $query_parent_topic_id=$array['topic_id'];
+    $query_parent_topic_id=$array['topicId'];
     $query_question=$array['question'];
-    $query_versions=implode('|',$array['versions']);
-    $query_answer=implode('|',$array['answer']);
-    $query_card_price=$array['card_price'];
+    $query_card_price=$array['cardPrice'];
+    $query_responses=$array['responses'];
     $check_card=queryMysql(
             "SELECT * FROM cards "
-          . "WHERE parent_topic_id='$query_parent_topic_id' "
+          . "WHERE parentTopicId='$query_parent_topic_id' "
           . "AND question='$query_question'"
             );
     
@@ -144,10 +158,17 @@ class App {
       queryMysql(
               "INSERT INTO cards VALUES("
                                       . "NULL, '$query_parent_topic_id', "
-                                      . "'$query_question', '$query_versions', "
-                                      . "'$query_answer', '$query_card_price'"
+                                      . "'$query_question', '$query_card_price'"
                                       . ")"
               );
+      
+      $query_card_id=queryMysql(
+            "SELECT cardId FROM cards "
+          . "WHERE parentTopicId='$query_parent_topic_id' "
+          . "AND question='$query_question'"
+            );
+      
+            $this->add_responses($query_responses, $query_card_id);
       
       return $this->viewTopics;
     } else {
@@ -157,28 +178,56 @@ class App {
   }
   
   
+  public function add_responses($array, $card_id) {
+    foreach ($array as $value) {
+      $query_answer=$value['answer'];
+      $query_is_right=$value['isRight'];
+      queryMysql(
+              "INSERT INTO rsponses VALUES("
+                                        . "'$card_id', "
+                                        . "'$query_answer', '$query_is_right'"
+                                        . ")"
+              );
+    }
+    
+  }
+  
+  
   public function edit_card($array) {
-    $query_card_id=$array['card_id'];
+    $query_card_id=$array['cardId'];
     $query_question=$array['question'];
-    $query_versions=implode('|',$array['versions']);
-    $query_answer=implode('|',$array['answer']);
-    $query_card_price=$array['card_price'];
+    $query_card_price=$array['cardPrice'];
+    $query_responses=$array['responses'];
     queryMysql(
             "UPDATE cards "
           . "SET question='$query_question', "
-          . "versions='$query_versions', "
-          . "answer='$query_answer', "
-          . "card_price='$query_card_price' "
-          . "WHERE card_id='$query_card_id'"
+          . "cardPrice='$query_card_price' "
+          . "WHERE cardId='$query_card_id'"
             );
     
+            $this->edit_responses($query_responses, $query_card_id);
+    
     return $this->viewTopics;
+  }
+  
+  
+  public function edit_responses($array, $card_id) {
+    foreach ($array as $value) {
+      $query_answer=$value['answer'];
+      $query_is_right=$value['isRight'];
+    queryMysql(
+            "UPDATE rsponses "
+          . "SET answer='$query_answer', "
+          . "isRighte='$query_is_right' "
+          . "WHERE parentCardId='$card_id'"
+            );
+    }
   }
   
   public function delete_card($card_id) {
     queryMysql(
             "DELETE FROM cards "
-          . "WHERE card_id='$card_id'"
+          . "WHERE cardId='$card_id'"
             );
     
     return $this->viewTopics;
